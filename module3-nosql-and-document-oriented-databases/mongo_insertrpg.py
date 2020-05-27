d8fd2fa0476054a355d05bafb8d4940577f35e8b
sweta sengupta <ssengupta801@gmail.com>
2020-05-27 17:32:40 -0500

    added comments

--- module3-nosql-and-document-oriented-databases/mongo_insertrpg.py
+++ module3-nosql-and-document-oriented-databases/mongo_insertrpg.py
@@ -1,3 +1,6 @@
+#link used: https://stackoverflow.com/questions/49221550/pandas-insert-a-dataframe-to-mongodb
+
+# I like sqlite best. I like the structure of it. I think and understand quicker when I can see rows and columns.
 
 import sqlite3
 import pandas as pd
