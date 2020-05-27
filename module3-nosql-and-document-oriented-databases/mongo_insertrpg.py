107ccbb28da65296ae9867eecfe29d6004c0afd4
sweta sengupta <ssengupta801@gmail.com>
2020-05-27 16:58:27 -0500

    updated assignment file

---
+++ module3-nosql-and-document-oriented-databases/mongo_insertrpg.py
@@ -0,0 +1,34 @@
+
+import sqlite3
+import pandas as pd
+import os
+from dotenv import load_dotenv
+import pymongo
+
+
+
+
+
+    # Create your connection.
+DB_FILEPATH = os.path.join(os.path.dirname(__file__),"..","module1-introduction-to-sql", "rpg_db.sqlite3")
+con = sqlite3.connect(DB_FILEPATH)
+data = pd.read_sql_query("SELECT * FROM charactercreator_character;", con)
+    
+load_dotenv()
+
+DB_USER = os.getenv("MONGO_USER")
+DB_PASSWORD = os.getenv("MONGO_PASSWORD")
+CLUSTER_NAME = os.getenv("MONGO_CLUSTER_NAME")
+
+connection_uri = f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority"
+client = pymongo.MongoClient(connection_uri)
+
+db = client.rpg_database # "test_database" or whatever you want to call it
+collection = db.rpg_test # "pokemon_test" or whatever you want to call it
+
+
+df_dict = data.to_dict("records") #covert to dictionary
+collection.insert_many(df_dict)
+
+print("DOCS:", collection.count_documents({})) #SELECT count(__) as row_count FROM my_table
+print(collection.count_documents({"name": "Aliquid iste optio reiciendi"}))#this also has the where condition
